03089a27b6fdd8d27e630caf1643d982f5ac4799
Jakub Konrad Minkiewicz <jakubcontact@icloud.com>
2022-08-04 18:20:23 +0100

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Jakub Minkiewicz on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
