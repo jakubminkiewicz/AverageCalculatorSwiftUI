3fba8a37f9b48a74994f9d5f2bc53b69d9b4ee33
Jakub Konrad Minkiewicz <jakubcontact@icloud.com>
2022-08-04 19:07:22 +0100

    - Add basic layout for calculator buttons - Add layout for input display

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".",".","="]
+    ]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("420")
+                        .foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                ForEach(buttons, id: \.self){ row in
+                    HStack (spacing: 12) {
+                        ForEach(row, id: \.self){ button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: buttonWidth(), height: buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(buttonWidth()/2)
+                        }
+                    }
+                }
+                
+                
+            }
+            .padding(.bottom)
+        }
+        
+        
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
